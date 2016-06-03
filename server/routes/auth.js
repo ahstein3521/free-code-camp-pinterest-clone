aaedfe107f960daed0ab9e1bfd334d9b2dd2cbc7
adamhs3521 <adam_stein@student.uml.edu>
2016-06-03 01:28:41 -0400

    initial commit

---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+
+module.exports = function(app){
+	const passport = require('passport');
+	const User=require('../models/user');
+
+   
+    app.get('/auth/twitter/callback',
+    	passport.authenticate('twitter', { failureRedirect: 'http://localhost:8080/fail'}),
+    		function(req,res){
+				res.redirect(`/user/${req.user.user.twitter_token}`)
+			})
+
+    
+    app.get('/auth/twitter',passport.authenticate('twitter'))
+
+}
